cade1c7e6b13f970f8db87ca203c8e778b84eed2
theohrogalski <rogalski.theo@gmail.com>
2026-05-12 21:48:11 -0400

    Update to networks

--- out/clang-release/target.o.d
+++ out/clang-release/target.o.d
@@ -1,6 +1,47 @@
 out/clang-release//target.o: target.cpp \
-  /home/rogal/omnet/omnetpp-6.0.1/include/omnetpp.h customMessage_m.h
+  /home/rogal/omnet/omnetpp-6.0.1/include/omnetpp.h customMessage_m.h \
+  ../inet4.5/src/inet/mobility/contract/IMobility.h \
+  ../inet4.5/src/inet/common/geometry/common/Coord.h \
+  ../inet4.5/src/inet/common/INETMath.h \
+  ../inet4.5/src/inet/common/INETDefs.h \
+  ../inet4.5/src/inet/common/precompiled_release.h \
+  ../inet4.5/src/inet/common/precompiled.h \
+  ../inet4.5/src/inet/opp_defines.h ../inet4.5/src/inet/features.h \
+  ../inet4.5/src/inet/common/Compat.h \
+  ../inet4.5/src/inet/common/InitStages.h \
+  ../inet4.5/src/inet/common/InitStageRegistry.h \
+  ../inet4.5/src/inet/common/geometry/common/Quaternion.h \
+  ../inet4.5/src/inet/common/geometry/common/EulerAngles.h \
+  ../inet4.5/src/inet/common/Units.h
 
 /home/rogal/omnet/omnetpp-6.0.1/include/omnetpp.h:
 
 customMessage_m.h:
+
+../inet4.5/src/inet/mobility/contract/IMobility.h:
+
+../inet4.5/src/inet/common/geometry/common/Coord.h:
+
+../inet4.5/src/inet/common/INETMath.h:
+
+../inet4.5/src/inet/common/INETDefs.h:
+
+../inet4.5/src/inet/common/precompiled_release.h:
+
+../inet4.5/src/inet/common/precompiled.h:
+
+../inet4.5/src/inet/opp_defines.h:
+
+../inet4.5/src/inet/features.h:
+
+../inet4.5/src/inet/common/Compat.h:
+
+../inet4.5/src/inet/common/InitStages.h:
+
+../inet4.5/src/inet/common/InitStageRegistry.h:
+
+../inet4.5/src/inet/common/geometry/common/Quaternion.h:
+
+../inet4.5/src/inet/common/geometry/common/EulerAngles.h:
+
+../inet4.5/src/inet/common/Units.h:
